515afdda85c6e9a13a40aa461833ebad29678777
piushroy7 <piushroy77@gmail.com>
2026-03-05 20:56:10 +0530

    if(else) statement
    dsa code

---
+++ program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main ()  {
+    int age;
+    cout<<"Enter your age";
+    cin>>age;
+ 
+    if(age>=18) {
+      cout<<"you can vote\n";
+    } else{
+      cout<<"you are under age\n";
+    }
+ }
